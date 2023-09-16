529c49f8063366a85d1b004034e0d44dd8e8ca70
Marco Antônio <marcomartins06@rede.ulbra.br>
2023-09-16 17:19:18 -0300

    fix: :bug: arrumando erros e skipando testes dos exemplos nas mkdocstrings

--- apilizer/verify.py
+++ apilizer/verify.py
@@ -7,67 +7,34 @@ async def check_swagger(uri: str) -> bool:
 
     Parameters:
         uri (str): The base URI of the API.
-        uri (str): The base URI of the API.
 
     Returns:
-        Returns `True` if the API has Swagger documentation, and `False` otherwise.
+        A dictionary containing the 'status' key with values 'success' or 'failure', and a 'message' key with detailed information.
 
     Examples:
         >>> import asyncio
-        >>> asyncio.run(check_swagger('http://127.0.0.1:8000'))
-        True
-        
+        >>> asyncio.run(check_swagger('http://127.0.0.1:8000')) # doctest: +SKIP
+        {'status': 'success', 'message': 'Swagger documentation found.'}
+
         >>> import asyncio
-        >>> asyncio.run(check_swagger('http://127.0.0.1:8000'))
-        False
+        >>> asyncio.run(check_swagger('http://127.0.0.1:8000')) # doctest: +SKIP
+        {'status': 'failure', 'message': 'Could not reach the API: ...'}
     """
     async with httpx.AsyncClient() as client:
         try:
             response = await client.get(f'{uri}/swagger.json')
-            return response.status_code == 200
+            if response.status_code == 200:
+                return {
+                    'status': 'success',
+                    'message': 'Swagger documentation found.',
+                }
+            else:
+                return {
+                    'status': 'failure',
+                    'message': f'Unexpected status code: {response.status_code}',
+                }
         except httpx.RequestError as exc:
-            print(f'Request error: {exc}')
-            return False
-
-
-async def check_post_201(uri: str) -> dict:
-    """
-    Check if the API's POST endpoints return the 201 status code.
-
-    Parameters:
-        uri (str): The base URI of the API.
-
-    Returns:
-        A dictionary mapping endpoints to their response status when a POST request is made.
-
-    Examples:
-        >>> import asyncio
-        >>> asyncio.run(check_post_201('http://127.0.0.1:8000'))
-        {'http://127.0.0.1:8000/endpoint1': 201, 'http://127.0.0.1:8000/endpoint2': 'Error: ...'}
-        
-        >>> import asyncio
-        >>> asyncio.run(check_post_201('http://127.0.0.1:8000'))
-        {'http://127.0.0.1:8000/endpoint1': 201, 'http://127.0.0.1:8000/endpoint2': 201}
-    """
-    results = {}
-
-    async with httpx.AsyncClient() as client:
-        try:
-            response = await client.get(f'{uri}/swagger.json')
-            spec_dict = response.json()
-        except httpx.RequestError as exc:
-            print(f'Request error: {exc}')
-            return results
-
-        for path, path_item in spec_dict.get('paths', {}).items():
-            if 'post' in path_item:
-                endpoint_url = f'{uri}{path}'
-                post_data = {}
-
-                try:
-                    response = await client.post(endpoint_url, json=post_data)
-                    results[endpoint_url] = response.status_code
-                except httpx.RequestError as exc:
-                    results[endpoint_url] = f'Error: {exc}'
-
-    return results
+            return {
+                'status': 'failure',
+                'message': f'Could not reach the API: {exc}',
+            }
