2d1a4ab12e505363e0c1419dd1349e2c50e174df
Marco Antônio <marcomartins06@rede.ulbra.br>
2023-09-16 19:31:27 -0300

    feat: :sparkles: melhorando função check_swagger para testar swagger e openapi

--- apilizer/verify.py
+++ apilizer/verify.py
@@ -1,40 +1,47 @@
 import httpx
 
 
-async def check_swagger(uri: str) -> bool:
+async def check_swagger(uri: str) -> dict:
     """
-    Check if the given base URI of the API has Swagger documentation.
+    Check if the given base URI of an API has Swagger or OpenAPI documentation available at the default '/openapi.json' or '/swagger.json' endpoints.
 
     Parameters:
         uri (str): The base URI of the API.
 
     Returns:
-        A dictionary containing the 'status' key with values 'success' or 'failure', and a 'message' key with detailed information.
+        A dictionary containing the 'status' key with values 'success' or 'failure', and a 'message' key with detailed information on the status or errors encountered.
+
+    Raises:
+        httpx.RequestError: If there was an error making the request (note: this is handled within the function and reported in the return dict).
 
     Examples:
         >>> import asyncio
         >>> asyncio.run(check_swagger('http://127.0.0.1:8000')) # doctest: +SKIP
-        {'status': 'success', 'message': 'Swagger documentation found.'}
+        {'status': 'success', 'message': 'Swagger/OpenAPI documentation found at /openapi.json.'}
 
         >>> import asyncio
         >>> asyncio.run(check_swagger('http://127.0.0.1:8000')) # doctest: +SKIP
-        {'status': 'failure', 'message': 'Could not reach the API: ...'}
+        {'status': 'failure', 'message': 'Could not find Swagger/OpenAPI documentation.'}
     """
     async with httpx.AsyncClient() as client:
-        try:
-            response = await client.get(f'{uri}/swagger.json')
-            if response.status_code == 200:
-                return {
-                    'status': 'success',
-                    'message': 'Swagger documentation found.',
-                }
-            else:
-                return {
-                    'status': 'failure',
-                    'message': f'Unexpected status code: {response.status_code}',
-                }
-        except httpx.RequestError as exc:
-            return {
-                'status': 'failure',
-                'message': f'Could not reach the API: {exc}',
-            }
+        errors = []
+        for endpoint in ('openapi.json', 'swagger.json'):
+            try:
+                response = await client.get(f'{uri}/{endpoint}')
+                if response.status_code == 200:
+                    return {
+                        'status': 'success',
+                        'message': f'Swagger/OpenAPI documentation found at /{endpoint}.',
+                    }
+                else:
+                    errors.append(
+                        f'{response.status_code} Client Error: {response.reason_phrase} for url: {response.url}'
+                    )
+            except httpx.RequestError as exc:
+                errors.append(str(exc))
+                continue
+
+        return {
+            'status': 'failure',
+            'message': f'Could not find Swagger/OpenAPI documentation. Errors encountered: {errors}',
+        }
--- tests/test_verify.py
+++ tests/test_verify.py
@@ -7,7 +7,23 @@ from apilizer.verify import check_swagger
 
 
 @respx.mock
-def test_check_swagger_with_api_and_swagger():
+def test_check_swagger_with_openapi_endpoint():
+    respx.get('http://127.0.0.1:8000/openapi.json').mock(
+        return_value=httpx.Response(200, json={})
+    )
+
+    result = asyncio.run(check_swagger('http://127.0.0.1:8000'))
+    assert result == {
+        'status': 'success',
+        'message': 'Swagger/OpenAPI documentation found at /openapi.json.',
+    }
+
+
+@respx.mock
+def test_check_swagger_with_swagger_endpoint():
+    respx.get('http://127.0.0.1:8000/openapi.json').mock(
+        return_value=httpx.Response(404)
+    )
     respx.get('http://127.0.0.1:8000/swagger.json').mock(
         return_value=httpx.Response(200, json={})
     )
@@ -15,12 +31,15 @@ def test_check_swagger_with_api_and_swagger():
     result = asyncio.run(check_swagger('http://127.0.0.1:8000'))
     assert result == {
         'status': 'success',
-        'message': 'Swagger documentation found.',
+        'message': 'Swagger/OpenAPI documentation found at /swagger.json.',
     }
 
 
 @respx.mock
-def test_check_swagger_with_api_and_no_swagger():
+def test_check_swagger_with_api_and_no_documentation():
+    respx.get('http://127.0.0.1:8000/openapi.json').mock(
+        return_value=httpx.Response(404)
+    )
     respx.get('http://127.0.0.1:8000/swagger.json').mock(
         return_value=httpx.Response(404)
     )
@@ -28,22 +47,31 @@ def test_check_swagger_with_api_and_no_swagger():
     result = asyncio.run(check_swagger('http://127.0.0.1:8000'))
     assert result == {
         'status': 'failure',
-        'message': 'Unexpected status code: 404',
+        'message': "Could not find Swagger/OpenAPI documentation. Errors encountered: ['404 Client Error: Not Found for url: http://127.0.0.1:8000/openapi.json', '404 Client Error: Not Found for url: http://127.0.0.1:8000/swagger.json']",
     }
 
 
 @respx.mock
 def test_check_swagger_no_api():
+    respx.get('http://127.0.0.1:8000/openapi.json').mock(
+        side_effect=httpx.NetworkError('Error')
+    )
     respx.get('http://127.0.0.1:8000/swagger.json').mock(
         side_effect=httpx.NetworkError('Error')
     )
 
     result = asyncio.run(check_swagger('http://127.0.0.1:8000'))
     assert result['status'] == 'failure'
-    assert 'Could not reach the API' in result['message']
+    assert (
+        'Could not find Swagger/OpenAPI documentation. Errors encountered: ['
+        in result['message']
+    )
 
 
 def test_check_swagger_invalid_url():
     result = asyncio.run(check_swagger('invalid_url'))
     assert result['status'] == 'failure'
-    assert 'Could not reach the API:' in result['message']
+    assert (
+        'Could not find Swagger/OpenAPI documentation. Errors encountered: ['
+        in result['message']
+    )
