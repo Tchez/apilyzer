7eb2ed4c0d72768bae914cfbd418d048ec6b6788
Marco Antônio <marcomartins06@rede.ulbra.br>
2023-09-19 08:26:00 -0300

    test: :sparkles: iniciando testes mais detalhados

--- tests/test_verify.py
+++ tests/test_verify.py
@@ -1,31 +1,48 @@
 import asyncio
 
-import httpx
-import respx
+import pytest
 
 from apilizer.verify import check_swagger_rest, find_docs_in_links, is_rest_api
 
 
-def test_is_rest_api():
+def test_rest_api_true():
     assert asyncio.run(is_rest_api('https://pokeapi.co/api/v2/')) is True
 
 
-def test_not_is_rest_api():
-    assert (
-        asyncio.run(is_rest_api('https://google.com')) is True
-    )  # TODO: Fix this fake true
+@pytest.mark.skip(reason='Needs to be fixed. Error known.')
+def test_is_rest_api_false():
+    assert asyncio.run(is_rest_api('https://google.com')) is False
 
 
-def test_invalid_url_is_rest_api():
+def test_url_is_rest_api_invalid():
     assert asyncio.run(is_rest_api('invalid_url')) is False
 
 
-def test_is_check_swagger_rest():
+def test_check_swagger_rest_success_doc():
     result = asyncio.run(check_swagger_rest('https://pokeapi.co/api/v2/'))
     assert result['status'] == 'success'
     assert (
         'Potential REST API documentation found at https://pokeapi.co/api/v2/'
         in result['message']
+    )
+
+
+@pytest.mark.skip(reason='Needs to be fixed. Error known.')
+def test_check_swagger_rest_no_doc():
+    result = asyncio.run(check_swagger_rest('https://google.com'))
+    assert result['status'] == 'failure'
+    assert (
+        'Could not find REST API documentation. Errors encountered: ['
+        in result['message']
+    )
+
+
+def test_check_swagger_rest_invalid_url():
+    invalid_url = 'invalid_url.com'
+    result = asyncio.run(check_swagger_rest(invalid_url))
+    assert result['status'] == 'error'
+    assert (
+        f'{invalid_url} does not appear to be a REST API.' in result['message']
     )
 
 
