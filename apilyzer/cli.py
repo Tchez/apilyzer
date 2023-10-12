a3b77c0def90f9bda2fd96a763a21fd797a3daf3
Marco Antônio <marcomartins06@rede.ulbra.br>
2023-10-12 20:26:41 -0300

    feat: :sparkles: melhorando o CLI

--- apilyzer/cli.py
+++ apilyzer/cli.py
@@ -1,10 +1,9 @@
 import asyncio
 
 from rich.console import Console
-from typer import Argument, Context, Typer
+from typer import Argument, Context, Option, Typer
 
 from apilyzer.verify import (
-    _is_json_rest_api,
     analyze_api_maturity,
     check_documentation_json,
     estimate_rate_limit,
@@ -20,27 +19,25 @@ def main(
 ):
     message = """
 
-Forma de uso: [green]apilyzer [SUBCOMANDO] [ARGUMENTOS][/]
+Forma de uso: [green]apilyzer [SUBCOMANDO] [ARGUMENTOS] [FLAGS] [/]
 
-Atualmente, existem 2 subcomandos disponíveis para essa aplicação:
+Atualmente, existem 3 subcomandos disponíveis para essa aplicação:
 
 - [b]verify-rest[/]: Verifica se uma API REST está documentada com base na URL fornecida e caso esteja, retorna o retorno da API
 - [b]verify-maturity[/]: Analisa o nível de maturidade de uma API REST com base no modelo de maturidade de Richardson
 - [b]test-rate[/]: Efetua uma quantidade de requisições (100 por padrão) para a API com o objetivo de validar se a API tem um limite para a quantidade de requisições
 
+[b]Mais informações de cada comando:[/]
+
+[green]apilyzer verify-rest --help [/]
+[green]apilyzer verify-maturity --help [/]
+[green]apilyzer test-rate --help [/]
+
 [b]Exemplos de uso:[/]
 
-[green]apilyzer verify-rest [/]
-[green]apilyzer verify-maturity [/]
-[green]apilyzer test-rate [/]
-
-
-[green]apilyzer verify-rest https://petstore.swagger.io v2/swagger[/]
+[green]apilyzer verify-rest https://petstore.swagger.io -e v2/swagger[/]
 [green]apilyzer verify-maturity https://picpay.github.io/picpay-docs-digital-payments/swagger/checkout.json[/]
 [green]apilyzer test-rate https://petstore.swagger.io/v2/pet[/]
-
-
-[b]Para mais informações: [yellow]apilyzer --help[/]
 """
     if ctx.invoked_subcommand:
         return
@@ -52,12 +49,14 @@ def verify_rest(
     url: str = Argument(
         'http://127.0.0.1:8000', help='URL of the API to verify'
     ),
-    endpoint: str = Argument(
+    doc_endpoint: str = Option(
         None,
-        help='Endpoint of the API to verify. If not provided, we will try to identify it with the base URL alone',
+        '--doc-endpoint',
+        '-e',
+        help='Endpoint of the API documentation. If not provided, we will try to identify it with the base URL alone',
     ),
 ):
-    result = asyncio.run(check_documentation_json(url, endpoint))
+    result = asyncio.run(check_documentation_json(url, doc_endpoint))
     console.print(result)
 
 
@@ -66,8 +65,14 @@ def verify_maturity(
     url: str = Argument(
         'http://127.0.0.1:8000', help='URL of the API to verify'
     ),
+    doc_endpoint: str = Option(
+        None,
+        '--doc-endpoint',
+        '-e',
+        help='Endpoint of the API documentation. If not provided, we will try to identify it with the base URL alone',
+    ),
 ):
-    result = asyncio.run(analyze_api_maturity(url))
+    result = asyncio.run(analyze_api_maturity(url, doc_endpoint))
     console.print(result)
 
 
@@ -82,7 +87,3 @@ def test_rate(
 ):
     result = asyncio.run(estimate_rate_limit(url, rate))
     console.print(result)
-
-
-if __name__ == '__main__':
-    app()
